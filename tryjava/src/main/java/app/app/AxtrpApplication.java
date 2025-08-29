ae29b6e3d96950c1294129872f83344710370fb8
AxoTheRedProtogen <alejo.ortiza2006@gmail.com>
2025-08-28 21:00:57 -0500

    initial commit

---
+++ tryjava/src/main/java/app/app/AxtrpApplication.java
@@ -0,0 +1,13 @@
+package app.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxtrpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxtrpApplication.class, args);
+	}
+
+}
