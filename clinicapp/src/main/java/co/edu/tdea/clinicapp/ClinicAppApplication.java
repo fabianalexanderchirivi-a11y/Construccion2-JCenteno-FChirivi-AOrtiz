747f145883061e4dd3bf0b5b0114f47839c98b53
fabianalexanderchirivi-a11y <fabianalexanderchirivi@gmail.com>
2025-11-18 01:49:53 -0500

    Limit entity scan to active JPA packages

--- clinicapp/src/main/java/co/edu/tdea/clinicapp/ClinicAppApplication.java
+++ clinicapp/src/main/java/co/edu/tdea/clinicapp/ClinicAppApplication.java
@@ -2,7 +2,6 @@ package co.edu.tdea.clinicapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
@@ -26,16 +25,20 @@ import co.edu.tdea.clinicapp.config.JwtProperties;
                         type = FilterType.REGEX,
                         pattern = "co\\.edu\\.tdea\\.clinicapp\\.adapter\\.out\\.persistence\\.jpa\\.clinical\\..*")
         })
-@EnableJpaRepositories(
-        basePackages = "co.edu.tdea.clinicapp.adapter.out.persistence.jpa",
+@EnableJpaRepositories(basePackages = "co.edu.tdea.clinicapp.adapter.out.persistence.jpa",
         excludeFilters = @ComponentScan.Filter(
                 type = FilterType.REGEX,
                 pattern = "co\\.edu\\.tdea\\.clinicapp\\.adapter\\.out\\.persistence\\.jpa\\.clinical\\..*"))
-@EntityScan(
-        basePackages = "co.edu.tdea.clinicapp",
-        excludeFilters = @EntityScan.Filter(
-                type = FilterType.REGEX,
-                pattern = "co\\.edu\\.tdea\\.clinicapp\\.adapter\\.out\\.persistence\\.jpa\\.clinical\\..*"))
+@org.springframework.boot.autoconfigure.domain.EntityScan(basePackages = {
+        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.auth",
+        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.appointment",
+        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.billing",
+        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.catalog",
+        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.clinicalhistory",
+        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.nursing",
+        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.order",
+        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.patient"
+})
 @EnableConfigurationProperties(JwtProperties.class)
 public class ClinicAppApplication {
     public static void main(String[] args) {
