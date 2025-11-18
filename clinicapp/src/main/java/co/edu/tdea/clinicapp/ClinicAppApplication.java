45f96842f5e8aa19dd8fe62805b27a6fe089fd7d
fabianalexanderchirivi-a11y <fabianalexanderchirivi@gmail.com>
2025-11-18 01:59:28 -0500

    Scan medication and catalog JPA entities

--- clinicapp/src/main/java/co/edu/tdea/clinicapp/ClinicAppApplication.java
+++ clinicapp/src/main/java/co/edu/tdea/clinicapp/ClinicAppApplication.java
@@ -35,10 +35,15 @@ import co.edu.tdea.clinicapp.config.JwtProperties;
         "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.billing",
         "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.catalog",
         "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.clinicalhistory",
+        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.diagnostic",
+        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.history",
+        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.medication",
         "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.nursing",
         "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.order",
         "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.patient",
-        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.user"
+        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.procedure",
+        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.user",
+        "co.edu.tdea.clinicapp.adapter.out.persistence.jpa.vitals"
 })
 @EnableConfigurationProperties(JwtProperties.class)
 public class ClinicAppApplication {
